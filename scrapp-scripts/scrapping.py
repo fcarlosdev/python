a48c515975899214c68d78feed588da91abeb742
fcarlos.silva <fcarlos.silva@dataprev.gov.br>
2019-02-25 08:25:52 -0300

    Improve the progress bar.

--- scrapp-scripts/scrapping.py
+++ scrapp-scripts/scrapping.py
@@ -7,7 +7,7 @@ links = []
 count = 1
 progress = ""
 progress_msg = ""
-for page in range(1,3):
+for page in range(1, 18):
     url = "https://www.mairovergara.com/category/como-se-diz-em-ingles/page/" + str(page) + "/"
     print "Extracting content from %s " % url
     progress_msg = "Extracting => ["
@@ -20,33 +20,19 @@ for page in range(1,3):
     for item in search_result:
         if (item.text.find('Como se diz')) > -1:
             links.append(item)
-        progress = (progress_msg+ "." * count) + "]("+str(((count*100)/total))+"%)"
+        progress = (progress_msg + ('.' * count) + (' ' * (total - count) + ']') + "("+str(((count*100)/total))+"%)")
         sys.stdout.write('\r'+progress)
-        time.sleep(.2)
+        time.sleep(.05)
         count += 1
     print "\n"
     progress = ""
     count = 1
 
-print links
-
-
-
-# output = []
-# for item in sentences:
-#   output.append(str(item))
-
-# with open('scrapp.txt', 'w') as f:
-#     for item in sentences:
-#         if (item.text.find('Como se diz')) > -1:
-#             f.write(str(item))
-
-
-# for x in sentences:
-#   # print x.text.encode('utf-8')
-#   if (x.text.find('Como se diz')) > -1:
-#     print x
-
-# links = soup.find_all('a', class_='page')
-#
 # print links
+
+
+with open('extracted_info.txt', 'w') as f:
+    for item in links:
+        if (item.text.find('Como se diz')) > -1:
+            f.write(str(item))
+    f.close()
